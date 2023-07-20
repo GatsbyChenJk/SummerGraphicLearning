9399be409aa631b971ccb8483a1d1528a95c9db3
GatsbyChenJk <128161055+GatsbyChenJk@users.noreply.github.com>
2023-07-20 15:03:25 +0800

    Add files via upload

---
+++ CppLearning/Composite_Mode/Client.cpp
@@ -0,0 +1,28 @@
+﻿#include "Composite.h"
+
+
+int main()
+{
+	Component* singleComp = new SingleComponent;
+	ClientCheckCompnonetType(singleComp);
+	if (!singleComp->IsComposite())
+	{
+		std::cout << "Client : create a composite which is single" << std::endl;
+	}
+
+	Component* complexComp1 = new ComplexComponent;
+	Component* complexComp2 = new ComplexComponent;
+	
+	complexComp1->Add(complexComp1);
+	complexComp2->Add(complexComp2);
+	complexComp1->Remove(complexComp1);
+	ClientCheckCompnonetType(complexComp2);
+	if (complexComp1->IsComposite())
+	{
+		std::cout << "Client : create a composite which is complex" << std::endl;
+	}
+	//注意最后释放内存
+	delete singleComp;
+	delete complexComp1;
+	delete complexComp2;
+}
---
+++ CppLearning/Composite_Mode/Composite.h
@@ -0,0 +1,74 @@
+﻿#pragma once
+#include <iostream>
+#include <list>
+#include <string>
+
+ 
+class Component
+{
+protected:
+	Component* Entity;
+public:
+	void SetComponent(Component* component)
+	{
+		this->Entity = component;
+	}
+	Component* GetComponent() const
+	{
+		return this->Entity;
+	}
+	//避免释放资源时派生类资源不能正常释放
+	virtual ~Component()
+	{}
+	//特定派生类需要重写方法，因此基类应将方法设为虚方法
+	virtual void Add(Component* AddComponent){}
+
+	virtual void Remove(Component* RemoveComponent){}
+	//使用const关键字保证不会对变量值修改
+	virtual bool IsComposite() const
+	{
+		return false;
+	}
+
+	virtual std::string ComponentInfo() const = 0;
+
+};
+
+class SingleComponent : public Component
+{
+public:
+	std::string ComponentInfo() const override
+	{
+		return "This is a Single Component";
+	}
+};
+
+class ComplexComponent : public Component
+{
+protected :
+	std::list<Component*> ComplexEntity;
+public :
+	void Add(Component* component) override
+	{
+		this->ComplexEntity.push_back(component);
+		SetComponent(this);
+	}
+	void Remove(Component* component) override
+	{
+		this->ComplexEntity.remove(component);
+		SetComponent(this);
+	}
+	bool IsComposite() const override
+	{
+		return true;
+	}
+	std::string ComponentInfo() const override
+	{
+		return "This is a complex component";
+	}
+};
+
+void ClientCheckCompnonetType(Component* checkComp)
+{
+	std::cout << checkComp->ComponentInfo() << std::endl;
+}
